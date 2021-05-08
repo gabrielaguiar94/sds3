8a487a9d661b732c97343fa3b7e3dea13f81cfad
Gabriel Aguiar <Gabriel Aguiar@DESKTOP-RKTB36F>
2021-05-08 14:16:20 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/gabrielaguiar94" target="_blank" rel="noreferrer">Gabriel Aguiar</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/graguiar" target="_blank" rel="noreferrer">@graguiar</a></small></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;

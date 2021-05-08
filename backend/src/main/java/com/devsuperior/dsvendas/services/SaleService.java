155ebfac4d3a5ac7d7edd233f3e0fc35bcdb16cf
Gabriel Aguiar <Gabriel Aguiar@DESKTOP-RKTB36F>
2021-05-08 16:44:26 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
@@ -22,10 +26,23 @@ public class SaleService {
 
 	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable) {
-		//AO CHAMAR O SELLER REPOSITORY SÃO EVITADAS AS INTERAÇÕES REPEITDAS NO BANCO DE DADOS, POIS O JPA ACESSO 
-		//OBJETOS QUE ESTARÃO EM MEMÓRIA
+		// AO CHAMAR O SELLER REPOSITORY SÃO EVITADAS AS INTERAÇÕES REPEITDAS NO BANCO
+		// DE DADOS, POIS O JPA ACESSO
+		// OBJETOS QUE ESTARÃO EM MEMÓRIA
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+
 	}
 }
